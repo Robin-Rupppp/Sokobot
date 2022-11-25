297a6c14ea65857796f4970f4badad04cbd84748
Robin-Rupppp <Axo.Axolo@proton.me>
2022-11-25 16:54:25 +0100

    Made Play Area

---
+++ src/main/java/ch/Robin_Rupp/Game.java
@@ -0,0 +1,18 @@
+package ch.Robin_Rupp;
+
+public class Game {
+
+    private final String PlayArea = """
+            ▥▥▥▥▥▥▥▥▥▥▥▥▥▥
+            ▥▢▢▢▢▢▢▢▢▢▢▢▢▥
+            ▥▢▢▢▢▢▢▢▢▢▢▢▢▥
+            ▥▢▢▢▢▢▢▢▢▢▢▢▢▥
+            ▥▢▢▢▢▢▢▢▢▢▢▢▢▥
+            ▥▢▢▢▢▢▢▢▢▢▢▢▢▥
+            ▥▥▥▥▥▥▥▥▥▥▥▥▥▥
+            """;
+    public void Play() {
+        System.out.println(PlayArea);
+
+    }
+}
--- src/main/java/ch/Robin_Rupp/Main.java
+++ src/main/java/ch/Robin_Rupp/Main.java
@@ -2,6 +2,7 @@ package ch.Robin_Rupp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game game = new Game();
+        game.Play();
     }
 }
